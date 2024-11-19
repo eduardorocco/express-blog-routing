62478922f13e37d5af4b9ae18c362c5d681b8665
eduardorocco <eduardo_rocco@hotmail.it>
2024-11-19 16:36:48 +0100

    REFACTOR show using slug

--- routers/posts.js
+++ routers/posts.js
@@ -9,20 +9,32 @@ router.get('/', (req, res) => {
     res.json(posts)
 })
 
-// show con id 
-router.get('/:id',(req, res) => {
-    const id = parseInt(req.params.id)
-    const post = posts.find((el) => el.id === id)
+// show con id////////////////
 
-    if (post){
-        console.log(`Ecco il post con id: ${id}`)
+// router.get('/:id',(req, res) => {
+//     const id = parseInt(req.params.id)
+//     const post = posts.find((el) => el.id === id)
+
+//     if (post){
+//         console.log(`Ecco il post con id: ${id}`)
+//         res.json(post)
+//     } else {
+//         res.json({message: `Post con id: ${id} non trovato`})
+//     }
+// })
+
+// show con slug
+router.get('/:slug', (req, res) => {
+    const slug = req.params.slug
+    const post = posts.find((el) => el.slug === slug)
+
+    if(post){
+        console.log(`Ecco il post con slug: ${slug}`);
         res.json(post)
     } else {
-        res.json({message: 'Post non trovato'})
+        res.json({message: `Post con slug ${slug} non trovato`})
     }
 })
-
-//
 
 // store 
 router.post('/', (req, res) => {

0e8c05d7158fdc0a40e85d85c052dceba0320364
eduardorocco <eduardo_rocco@hotmail.it>
2024-11-19 16:30:06 +0100

    REFACTOR using router

--- app.js
+++ app.js
@@ -1,56 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const posts = require('./data/posts.js')
-
-
+const postRouter = require('./routers/posts.js');
 
 app.use(express.static('public'))
 
-// console.log(posts);
-
-///////////CRUD/////////////////
-
-// index
-app.get('/posts', (req, res) => {
-    res.json(posts)
-})
-
-// show
-app.get('/posts/:id',(req, res) => {
-    const id = parseInt(req.params.id)
-    const post = posts.find((el) => el.id === id)
-
-    if (post){
-        console.log(`Ecco il post con id: ${id}`)
-        res.json(post)
-    } else {
-        res.json({message: 'Post non trovato'})
-    }
-})
-
-// store 
-app.post('/posts', (req, res) => {
-    res.send('Creo un nuovo post')
-})
-
-// update
-app.put('/posts/:id', (req, res) => {
-    const id = req.params.id
-    res.send(`Aggiorno il post con id: ${id}`)
-})
-
-// modify
-app.patch('/posts/:id', (req, res) => {
-    const id = req.params.id
-    res.send(`Modifico il post con id: ${id}`)
-})
-
-// destroy
-app.delete('/posts/:id', (req, res) => {
-	const id = req.params.id
-	res.send(`Elimino il post con id: ${id}`)
-})
+app.use('/posts', postRouter)
 
 app.listen(port,() => {
     console.log(`Server listening on port: ${port}`);
--- routers/posts.js
+++ routers/posts.js
@@ -1,3 +1,50 @@
 const express = require('express')
 const router = express.Router()
 const posts = require('../data/posts.js')
+
+///////////CRUD/////////////////
+
+// index
+router.get('/', (req, res) => {
+    res.json(posts)
+})
+
+// show con id 
+router.get('/:id',(req, res) => {
+    const id = parseInt(req.params.id)
+    const post = posts.find((el) => el.id === id)
+
+    if (post){
+        console.log(`Ecco il post con id: ${id}`)
+        res.json(post)
+    } else {
+        res.json({message: 'Post non trovato'})
+    }
+})
+
+//
+
+// store 
+router.post('/', (req, res) => {
+    res.send('Creo un nuovo post')
+})
+
+// update
+router.put('/:id', (req, res) => {
+    const id = req.params.id
+    res.send(`Aggiorno il post con id: ${id}`)
+})
+
+// modify
+router.patch('/:id', (req, res) => {
+    const id = req.params.id
+    res.send(`Modifico il post con id: ${id}`)
+})
+
+// destroy
+router.delete('/:id', (req, res) => {
+	const id = req.params.id
+	res.send(`Elimino il post con id: ${id}`)
+})
+
+module.exports = router

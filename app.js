c7010ddb06f3911bb3a92b26318c80ddac9e21b0
eduardorocco <eduardo_rocco@hotmail.it>
2024-11-19 16:04:41 +0100

    ADD index and show

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+const posts = require('./posts.js')
 app.use(express.static('public'))
+
+// console.log(posts);
+
+///////////CRUD/////////////////
+
+// index
+app.get('/', (req, res) => {
+    res.json(posts)
+})
+
+// show
+app.get('/:id',(req, res) => {
+    const id = parseInt(req.params.id)
+    const post = posts.find((el) => el.id === id)
+
+    if (post){
+        console.log(`Ecco il post con id: ${id}`)
+        res.json(post)
+    } else {
+        res.json({message: 'Post non trovato'})
+    }
+})
 
 
 app.listen(port,() => {
--- posts.js
+++ posts.js
@@ -43,4 +43,8 @@ const posts = [
   },
 ];
 
+posts.forEach((array,index) => {
+  array.id = index + 1
+})
+
 module.exports = posts;
